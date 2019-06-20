6524e6670fe27e4d61a10e9f8256f00bb03d9f5e
Max Wilets <mwilets@gmail.com>
2019-06-20 13:57:04 -0600

    test implementation

--- test.js
+++ test.js
@@ -1,4 +1,43 @@
 var tester = require('acme-challenge-test');
+const {google} = require('googleapis');
+var dns = google.dns('v1');
+
+authorize(function(authClient) {
+	var request = {
+	  // Identifies the project addressed by this request.
+	  project: 'east-high-support',  // TODO: Update placeholder value.
+  
+	  resource: {
+		// TODO: Add desired properties to the request body.
+	  },
+  
+	  auth: authClient,
+	};
+  
+	dns.managedZones.create(request, function(err, response) {
+	  if (err) {
+		console.error(err);
+		return;
+	  }
+  
+	  // TODO: Change code below to process the `response` object:
+	  console.log(JSON.stringify(response, null, 2));
+	});
+  });
+  
+  function authorize(callback) {
+	google.auth.getApplicationDefault(function(err, authClient) {
+	  if (err) {
+		console.error('authentication failed: ', err);
+		return;
+	  }
+	  if (authClient.createScopedRequired && authClient.createScopedRequired()) {
+		var scopes = ['https://www.googleapis.com/auth/cloud-platform'];
+		authClient = authClient.createScoped(scopes);
+	  }
+	  callback(authClient);
+	});
+  }
 // The dry-run tests can pass on, literally, 'example.com'
 // but the integration tests require that you have control over the domain
 //code for GCLOUD
